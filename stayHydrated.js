9c7495418c521826a426d549beb0261078431d2f
jcruzcode <jcruz.code@gmail.com>
2023-09-07 17:06:29 -0800

    8 kyu - Keep Hydrated!

---
+++ stayHydrated.js
@@ -0,0 +1,6 @@
+/* 8 kyu - Basic Mathematical Operations */
+function litres(time) {
+    // Return the number of litres Nathan will drink, 
+    // rounded to the smallest value.
+    return Math.floor(0.5 * time);
+}
