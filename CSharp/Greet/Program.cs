b6e2d51d3ec3da0a80ab491a51c5f544d281ce61
jcruzcode <jcruz.code@gmail.com>
2023-09-16 22:48:39 -0800

    Solved two 8 kyu katas with C#

---
+++ CSharp/Greet/Program.cs
@@ -0,0 +1,15 @@
+﻿// 8 kyu - Grasshopper - Personalized Message
+
+Console.WriteLine(Kata.Greet("Jonathan", "Bobby")); // Hello guest
+Console.WriteLine(Kata.Greet("Bobby", "Bobby")); // Hello boss
+public class Kata
+{
+  public static string Greet(string name, string owner)
+  {
+    if ( name == owner ) {
+      return "Hello boss";
+    } else {
+      return "Hello guest";
+    }
+  }  
+}
---
+++ CSharp/Multiply/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 8 kyu - Simple multiplication */
+Console.WriteLine(Multiplier.Multiply(3)); // 27
+Console.WriteLine(Multiplier.Multiply(2)); // 16
+public class Multiplier
+{
+  public static int Multiply(int x) 
+  {
+    return x % 2 == 0 ? 8 * x : 9 * x;
+  }
+}
