f38fe669b4ffd8b0dff528c7f3e0e7e994942477
jcruzcode <jcruz.code@gmail.com>
2023-09-18 00:02:14 -0800

    Solution for Sum The Strings (8 kyu)

---
+++ CSharp/StringsSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.WriteLine(Solution.Program.StringsSum("25", "10"));
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static string StringsSum(string s1, string s2)
+    {
+      if (s1 == "")
+        s1 = "0";
+      
+      if (s2 == "")
+        s2 = "0";
+      
+      int sum = Convert.ToInt32(s1) + Convert.ToInt32(s2);
+      
+      return sum.ToString();
+    }
+  }
+}
