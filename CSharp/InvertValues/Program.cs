53724f8be3a263f55251b13213ad1b66fcaa5109
jcruzcode <jcruz.code@gmail.com>
2023-09-18 09:53:00 -0800

    8 kyu - Invert values

---
+++ CSharp/InvertValues/Program.cs
@@ -0,0 +1,29 @@
+﻿// 8 kyu - Insert Values
+using System.Linq;
+
+int[] numbers = new int[] { 1, 2, 3 , 4 , 5 };  
+foreach( int number in Solution.ArraysInversion.InvertValues(numbers) )
+{
+    Console.WriteLine(number);
+}
+
+namespace Solution
+{
+  public static class ArraysInversion
+  {
+    public static int[] InvertValues(int[] input)
+    {
+      int tempNumber = 0;
+      int index = 0;
+      
+      foreach ( int number in input )
+      {
+        tempNumber = number;
+        input[index] = -number;
+        index++;
+      }
+      
+      return input;
+    }
+  }
+}
