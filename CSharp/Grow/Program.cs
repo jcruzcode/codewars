1db009bae482cbd48205270fff5b06a0d43e78d8
jcruzcode <jcruz.code@gmail.com>
2023-09-17 21:17:14 -0800

    Solution for Reduce but Grow (8kyu)

---
+++ CSharp/Grow/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 8 kyu - Beginner - Reduce but Grow */
+int[] numbers = new int[] { 1,2,3,4,5 };
+Console.WriteLine(Kata.Grow(numbers)); // 120
+public class Kata
+{
+  public static int Grow(int[] x)
+  {
+    int product = 1;
+    
+    foreach ( int number in x )
+    {
+      product *= number;
+    }
+    
+    return product;
+  }
+}
