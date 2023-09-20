62aca3f8a2845b97eb5df238917a7b88b5b33868
jcruzcode <jcruz.code@gmail.com>
2023-09-20 12:22:46 -0800

    Volume of Cuboid (8 kyu)

---
+++ CSharp/VolumeOfCuboid/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(Kata.GetVolumeOfCuboid( 10.2, 5.3124, 3.14));
+public class Kata {
+  /****************************************************
+  
+  GetVolumeOfCuboid calculates and returns the volume
+  of a cuboid based on the three parameters of 
+  data type double: length, width, and height.
+  
+  *****************************************************/
+  public static double GetVolumeOfCuboid(double length, double width, double height) {
+    return length * width * height;
+  }
+}
