8d2677d554f1c9dfce28dca18b59df4637a5ea38
jcruzcode <jcruz.code@gmail.com>
2023-09-19 18:08:30 -0800

    8 kyu - MakeUpperCase

---
+++ CSharp/MakeUpperCase/Program.cs
@@ -0,0 +1,14 @@
+﻿  // 8 kyu - MakeUpperCase
+
+  Console.WriteLine(Kata.MakeUpperCase("hello world"));
+  public class Kata
+  {
+        /*
+          This method returns a string in uppercase.
+        */
+        public static string MakeUpperCase(string str)
+        {
+             return str.ToUpper();
+        }
+
+  }
