e6ce6c37b2a81f6e4f845cb58f7cf170679529d9
jcruzcode <jcruz.code@gmail.com>
2023-09-22 15:52:58 -0800

    Fake Binary (8kyu)

---
+++ CSharp/FakeBinary/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(Kata.FakeBin("45385593107843568"));
+public class Kata
+{
+    public static string FakeBin(string x)
+    {
+     
+        string fakeBinary = "";
+        for (int i = 0; i < x.Length; i++)
+        {
+            if ( x[i] == '0' || x[i] == '1' || x[i] == '2' || x[i] == '3' || x[i] == '4' )
+                fakeBinary += "0";
+            else
+                fakeBinary += "1";
+        }
+        return fakeBinary;
+    }
+}
