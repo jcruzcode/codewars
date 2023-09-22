27a2443f62adfbd4a705b1a8e762aa89031a3b99
jcruzcode <jcruz.code@gmail.com>
2023-09-21 21:44:41 -0800

    What is between them? (8 kyu)

---
+++ CSharp/Between/Program.cs
@@ -0,0 +1,24 @@
+﻿// 8 kyu - What is between them?
+public class Kata {
+  
+  /*********************************************************
+   
+   The method Between takes two integers (a, b, where a < b) 
+   and return an array of all integers between the input 
+   parameters, including them.
+   
+  **********************************************************/
+  public static int[] Between(int a, int b) {
+    int[] numbers = new int[ b - a + 1 ];
+    int index = 0;
+    
+    for ( int i = a; i <= b; i++ ) 
+    {
+      numbers[index] = i; 
+      index++;
+    }
+    
+    return numbers;
+  }
+}
+
