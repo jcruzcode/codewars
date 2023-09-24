d4e418a8ea5e3722e977a8bf279dd7c1b589f7c4
jcruzcode <jcruz.code@gmail.com>
2023-09-23 20:49:08 -0800

    Keep up the hoop (8 kyu)

---
+++ CSharp/HoopCount/Program.cs
@@ -0,0 +1,10 @@
+﻿// 8 kyu - Keep up the hoop
+Console.WriteLine(Kata.HoopCount(11));
+Console.WriteLine(Kata.HoopCount(5));
+public class Kata
+{
+  public static string HoopCount(int n)
+  {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+  }
+}
