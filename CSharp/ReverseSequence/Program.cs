d8bf61a0942e68c0c113788543bd91c2fe9aab3b
jcruzcode <jcruz.code@gmail.com>
2023-09-24 20:03:55 -0800

    Reversed sequence (8 kyu)

---
+++ CSharp/ReverseSequence/Program.cs
@@ -0,0 +1,26 @@
+﻿// 8 kyu - Reversed Sequence
+using System;
+
+int[] test = Kata.ReverseSeq(5);
+
+foreach (int element in test)
+{
+    Console.WriteLine(element);
+}
+
+public static class Kata
+{
+  public static int[] ReverseSeq(int n)
+  {
+    int[] reverse = new int[n];
+    int index = 0;
+    
+    for ( int i = n; i >= 1; i-- )
+    {
+      reverse[index] = i;
+      index++;
+    }
+    
+    return reverse;
+  }
+}
