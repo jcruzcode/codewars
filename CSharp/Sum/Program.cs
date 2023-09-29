ec7c0c5253c93640adf3b5a3a57a7ac842a16c09
jcruzcode <jcruz.code@gmail.com>
2023-09-28 22:31:23 -0800

    Sum without the highest and lowest number (8 kyu)

---
+++ CSharp/Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+int[] numbers = { 6, 2, 1, 8, 10 };
+Console.WriteLine(Kata.Sum(numbers));
+public static class Kata
+{
+  public static int Sum(int[] numbers)
+  {
+    
+    if ( numbers == null || numbers.Length == 0 || numbers.Length == 1 )
+      return 0;
+    
+    Array.Sort(numbers);
+    
+    int sum = 0;
+    for ( int i = 1; i < numbers.Length - 1; i++ )
+      sum += numbers[i];
+    
+    return sum;
+  }
+}
