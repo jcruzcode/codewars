1f26d661ab5dd5da25c6d4dee7a346da0fda0354
jcruzcode <jcruz.code@gmail.com>
2023-10-06 09:59:55 -0800

    Grasshopper - Messi Goals (8 kyu)

---
+++ javascript/messiGoals.js
@@ -0,0 +1,6 @@
+// 8 kyu - Grasshopper - Messi Goals
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
