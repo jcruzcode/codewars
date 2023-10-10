6995240365da1664478bd556e67f6006950e0e3b
jcruzcode <jcruz.code@gmail.com>
2023-10-10 12:33:12 -0800

    Drink about (8 kyu)

---
+++ CSharp/Drink/Program.cs
@@ -0,0 +1,23 @@
+﻿// 8 kyu - Drink about
+public class Kata
+{
+  public static string PeopleWithAgeDrink(int old)
+  {
+    if ( old < 14 )
+    {
+      return "drink toddy";
+    }
+    else if ( old < 18 )
+    {
+      return "drink coke";
+    }
+    else if ( old < 21 )
+    {
+      return "drink beer";
+    } 
+    else
+    {
+      return "drink whisky";  
+    }
+  }
+}
