2c71bd54b8a3d90e956943e5686a9f8996df1402
jcruzcode <jcruz.code@gmail.com>
2023-12-22 12:30:53 -0900

    First non-repeating character (5 kyu)

---
+++ javascript/firstNonRepeatingLetter.js
@@ -0,0 +1,12 @@
+// First non-repeating character (5 kyu)
+function firstNonRepeatingLetter(s) {
+  const lower = s.toLowerCase();
+
+  for (let i = 0; i < lower.length; i++) {
+    if (lower.indexOf(lower[i]) === lower.lastIndexOf(lower[i])) {
+      return s[i];
+    }
+  }
+
+  return "";
+}
