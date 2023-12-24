42f046b87ee1bb089cd97cd98b97c1c5108f5704
jcruzcode <jcruz.code@gmail.com>
2023-12-23 15:07:39 -0900

    Refactor: Find the missing letter (6 kyu)

--- javascript/findMissingLetter.js
+++ javascript/findMissingLetter.js
@@ -1,11 +1,9 @@
 // Find the missing letter (6 kyu)
 function findMissingLetter(array) {
-  const codes = array.map((x) => x.charCodeAt(0));
+  const codes = array.map( x => x.charCodeAt(0) );
   let tmp = codes[0];
-  for (let i = 0; i < codes.length; i++) {
-    if (tmp != codes[i]) {
-      return String.fromCharCode(tmp);
-    }
+  for ( let code of codes ) {
+    if ( tmp != code ) return String.fromCharCode(tmp);
     tmp++;
   }
 }
