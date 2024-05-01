c97f8a45f985b3599ac36061799c46d78550f6bb
jcruzcode <jcruz.code@gmail.com>
2024-05-01 00:06:32 -0800

    ======== Function 2 - squaring an argument (8 kyu) ======== Now you have to write a function that takes an argument and returns the square of it.

---
+++ javascript/square.js
@@ -0,0 +1,22 @@
+/*
+======== Function 2 - squaring an argument (8 kyu) ========
+Now you have to write a function that takes an argument and returns the square 
+of it.
+
+1) Parameter - A number.
+2) Return - The number squared.
+3) Examples - see tests
+4) Pseudocode - see comments
+
+*/
+function square(number) {
+  // Return number squared
+  return number ** 2;
+}
+
+// Tests
+console.log(square(1), 1);
+console.log(square(2), 4);
+console.log(square(3), 9);
+console.log(square(4), 16);
+console.log(square(25), 625);
