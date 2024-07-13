7d70389982ef1c4cb8660c9a1e0f64be8b775eab
jcruzcode <jcruz.code@gmail.com>
2024-07-13 09:16:33 -0800

    ======= Factorial (7 kyu) ======= In mathematics the factorial of a non-negative integer n denoted by n! is the product of all positive integers less than or equal to n.
    For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
    
    Write a function to calculate factorial for a given input.
    
    If input is below 0 or above 12 throw an exception of type throw a RangeError (JavaScript).

--- javascript/factorials.js
+++ javascript/factorials.js
@@ -1,6 +1,6 @@
 /*
 ======= Factorial (7 kyu) =======
-In mathematics, the factorial of a non-negative integer n, denoted by n!, 
+In mathematics the factorial of a non-negative integer n denoted by n! 
 is the product of all positive integers less than or equal to n. 
 
 
