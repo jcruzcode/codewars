ceaefc5af8c229a0f84078935689e0f8540dd753
jcruzcode <jcruz.code@gmail.com>
2024-10-24 19:35:17 -0800

    ======== Did she say hallo? (8 kyu) ======== You received a whatsup message from an unknown number.
    Could it be from that girl/boy with a foreign accent you met yesterday evening?
    
    Write a simple function to check if the string contains the word hallo in different languages.
    
    These are the languages of the possible people you met the night before:
    
        hello - english
        ciao - italian
        salut - french
        hallo - german
        hola - spanish
        ahoj - czech republic
        czesc - polish
    
    Notes
    
        you can assume the input is a string.
        to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
        function should be case insensitive to pass the tests

--- javascript/validateHello.js
+++ javascript/validateHello.js
@@ -21,8 +21,6 @@ Notes
     you can assume the input is a string.
     to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
     function should be case insensitive to pass the tests
-
-
 */
 
 function validateHello(greetings) {
